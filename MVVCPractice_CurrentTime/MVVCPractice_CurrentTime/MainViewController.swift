2099e97bb081e18743edeac038e2799c4ac37c09
wiwi-git <duse34@hotmail.com>
2021-11-12 19:56:34 +0900

    CurrentTime 프로젝트 생성

---
+++ MVVCPractice_CurrentTime/MVVCPractice_CurrentTime/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  MVVCPractice_CurrentTime
+//
+//  Created by Emoticbox on 2021/11/12.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    var timeLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    var fetchTimeButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+}
